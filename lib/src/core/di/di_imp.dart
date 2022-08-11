ac5c238471139f01ca2f92e43c9c30daf0c2f807
Joao Ricardi <joaoricardi@MacBook-Pro-de-Joao.local>
2022-08-10 23:30:33 -0300

    comenco handler de di

---
+++ lib/src/core/di/di_imp.dart
@@ -0,0 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:weaather_flutter_app/src/core/di/di.dart';
+
+class DIImp implements DI {
+
+  final GetIt getIt = GetIt.instance;
+
+  @override
+  void init() {
+    //register modules
+  }
+
+}
