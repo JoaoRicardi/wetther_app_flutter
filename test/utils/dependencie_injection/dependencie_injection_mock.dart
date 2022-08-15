57706da785b2ef06bf97861e10ff0e71e4f69b03
Joao Ricardi <joaoricardi@MacBook-Pro-de-Joao.local>
2022-08-15 10:18:13 -0300

    subindo versao final ainda sem testes

--- test/utils/dependencie_injection/dependencie_injection_mock.dart
+++ test/utils/dependencie_injection/dependencie_injection_mock.dart
@@ -1,6 +1,4 @@
 import 'package:get_it/get_it.dart';
-// import 'package:isow/src/domain/repository/cep/cep_repository.dart';
-// import 'package:isow/src/presentation/initial/controller/initial_controller.dart';
 
 import '../mock/repository/repository_mocks.dart';
 
@@ -15,19 +13,14 @@ class DIMock {
   }
 
   _initPresentationModule(){
-    // getIt.registerLazySingleton<InitialController>(() => InitialController(getIt.get()));
   }
 
   _initCoreModule(){
-    // getIt.registerFactory<ILocalStorage>(() => LocalStorage());
-    //
-    // ///registe two diferentes factorys to get with and without authentication
-    // getIt.registerFactory<IHttpClientService>(() => HttpClient.withAuthOptions(),instanceName: "wAuth");
-    // getIt.registerFactory<IHttpClientService>(() => HttpClient.withAuthOptions(),instanceName: "wOutAuth");
+
   }
 
   _initDataModule(){
-    // getIt.registerLazySingleton<ICepRepository>(() => CepRepositoryMock());
+
   }
 
 }
